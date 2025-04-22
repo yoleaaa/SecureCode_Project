49bac0c6d4f12742854dfc49743c6a8347deed72
Yuan Olea <151420462+yoleaaa@users.noreply.github.com>
2025-04-22 16:34:00 -0500

    Update assignment5.py, added more vulnerabilities
    added some more errors for bandit to try and sense

--- assignment5.py
+++ assignment5.py
@@ -12,11 +12,28 @@ error_message = ''
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+## This code is from assignment 1
+def get_user_input():
+    user_input = input("Enter your name: ")
+    return user_input
+
+##This code is from assignment 1 
+def send_email(to, subject, body):
+    os.system(f'echo {body} | mail -s "{subject}" {to}')
+
+def save_to_db(data):
+    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    connection = pymysql.connect(**db_config)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
 try:
     with open('bank_data.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            ## Reset valid record and error message for each iteration
+            # Reset valid record and error message for each iteration
             valid_record = True
             error_message = ''
 
@@ -92,3 +109,5 @@ except ValueError:
                 print("ERROR: ValueError")
 except ZeroDivisionError: 
         print("ERROR:ZeroDivisionError")
+
+
